ecf64b343049019306fe77522f9972de23c5c58c
Maseu <m.wachowicz@protonmail.com>
2024-01-28 20:34:06 +0100

    add parsers

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,6 +4,13 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
+      "typescript",
+      "javascript",
+      "rust",
+      "html",
+      "css",
+      "yaml",
+      "json",
     })
   end,
 }
